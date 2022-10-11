acf286b26389f954b1c22a8d817d9d8073c61417
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-11 11:55:06 +0500

    Add the JavaScript functionality

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
