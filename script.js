e6e313ca13241ee674f546ec55b4b4aff8b0af56
ginabeki <ginabeki3@gmail.com>
2022-10-11 10:55:31 +0300

    Fix linters error

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
