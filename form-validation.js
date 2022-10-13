3a9fdcbf24a14e0492f37c6f4619588deddd0d1c
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-13 16:37:24 +0500

    Remove the liners errors

--- form-validation.js
+++ form-validation.js
@@ -4,22 +4,21 @@ const errorInfo = document.getElementById('error');
 
 function validateEmail() {
   const errorMessages = [];
-  const validRegex =
-    /^[a-z-0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z-0-9-]+(?:\.[a-z-0-9-]+)*$/;
+  const validRegex = /^[a-z-0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z-0-9-]+(?:\.[a-z-0-9-]+)*$/;
   if (!email.value.match(validRegex)) {
     errorMessages.push('Not sent, invalid email');
     errorInfo.innerHTML = `
       <span style='color:red; border:1px solid red; padding: 8px;'>${errorMessages.join(
-        ', '
-      )}</span>
+    ', ',
+  )}</span>
     `;
     return false;
   }
   errorMessages.push('Email sent successfully');
   errorInfo.innerHTML = `
       <span style='color:green; border:1px solid green; padding: 8px;'>${errorMessages.join(
-        ', '
-      )}</span>
+    ', ',
+  )}</span>
     `;
   return true;
 }
